2eaf95c5af580de6a92e78ccb44ba29cde0af4a4
volo_mori_9032 <scarycat105@gmail.com>
2025-05-21 13:36:07 +1200

    moew

---
+++ javascript/java.js
@@ -0,0 +1,24 @@
+document.getElementById("demo").innerHTML = "<h2>Hello World</h2>";
+
+
+let x, y, v;
+y = 10
+x = 11
+v = x * y
+document.getElementById("demo1").innerHTML = v;
+
+// I am a comment
+
+/*
+I am a
+long
+Comment
+*/
+
+/*
+let x = 5;
+x = 7;
+x = x + x;
+alert(x);
+document.getElementById("demo1").innerHTML = x;
+*/
